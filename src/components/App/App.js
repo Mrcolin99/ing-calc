f15d3046443d5c5f8a0f18667606e18862a4d5a0
Colin Ciervo <colinciervo@@gmail.com>
2023-09-15 16:34:06 -0400

    add styling to ingredients

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -50,27 +50,51 @@ function App() {
     <div className='calc-main'>
       <form>
         <input type='text' name='ingredient1-name' className='input-field'></input>
+        <label for='measurement1'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input onChange={ing1Change} type='text' name='ingredient1' className='input-field' />
         <br />
         <input type='text' name='ingredient2-name' className='input-field'></input>
+        <label for='measurement2'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input onChange={ing2Change} type='text' name='ingredient2' className='input-field' />
         <br />
         <input type='text' name='ingredient3-name' className='input-field'></input>
+        <label for='measurement3'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input onChange={ing3Change} type='text' name='ingredient3' className='input-field' />
         <br />
         <input type='text' name='ingredient4-name' className='input-field'></input>
+        <label for='measurement4'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input type='text' name='ingredient4' className='input-field' />
         <br />
         <input type='text' name='ingredient5-name' className='input-field'></input>
+        <label for='measurement5'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input type='text' name='ingredient5' className='input-field' />
         <br />
         <input type='text' name='ingredient6-name' className='input-field'></input>
+        <label for='measurement6'></label>
+        <select name='measurement' id='measurement'>
+          <option value='weight'>Weight</option>
+        </select>
         <input type='text' name='ingredient6' className='input-field' />
         <br />
       </form>
       <button className='calc-submit' onClick={submit}>
         <h3>Calculate</h3>
-        </button>
+      </button>
       {/* {solution()} */}
     </div>
   )
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,7 +1,20 @@
+import './Ingredients.css'
+
 function Ingredients() {
     return (
         <div>
-            <p>ingredients</p>
+
+            <h3 className='ing-title'>Create new ingredient</h3>
+            <form>
+                <label className='ing-label' for='ingredient'>Ingredient: </label>
+                <input className='ingredient' type='text' id='ingredient'></input>
+                <br />
+                <label className='weight-label' for='weight'>Total Weight: </label>
+                <input className='weight' type='text' id='weight'></input>
+                <br />
+                <label className='cost-label' for='cost'>Cost: </label>
+                <input className='cost' type='text' id='cost'></input>
+            </form>
         </div>
     )
 }
