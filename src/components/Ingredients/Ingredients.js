5e18c1797b0f986268ab97a6f22b4b1dbeb8450c
Colin Ciervo <colinciervo@@gmail.com>
2023-09-16 18:52:26 -0400

    Add Card component

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,6 +5,7 @@ function Ingredients() {
     const [name, setName] = useState('')
     const [weight, setWeight] = useState(0)
     const [cost, setCost] = useState(0)
+    const [list, setList] = useState([])
 
     const handleIngChange = (event) => {
         setName(event.target.value)
@@ -23,21 +24,42 @@ function Ingredients() {
         console.log(cost)
     }
 
+    const createNewIng = (event) => {
+        event.preventDefault()
+        const newIng = { name: name, weight: weight, cost: cost }
+        console.log(newIng)
+        setList(list => [...list, newIng])
+        console.log(list)
+        clearInputs()
+    }
+
+    const clearInputs = () => {
+        setName('')
+        setWeight(0)
+        setCost(0)
+    }
+
+    const listIng = () => {
+    }
+
 
     return (
         <div>
-            <h3 className='ing-title'>Create New Ingredient</h3>
-            <form>
-                <label className='ing-label' for='ingredient'>Ingredient Name: </label>
-                <input onChange={handleIngChange} className='ingredient' type='text' id='ingredient'></input>
-                <br />
-                <label className='weight-label' for='weight'>Total Weight: </label>
-                <input onChange={handleWeightChange} className='weight' type='text' id='weight'></input>
-                <br />
-                <label className='cost-label' for='cost'>Total Cost: $</label>
-                <input onChange={handleCostChange} className='cost' type='text' id='cost'></input>
-            </form>
-            <button className='create-new'>Add Ingredient</button>
+            <div>
+                <h3 className='ing-title'>Create New Ingredient</h3>
+                <form>
+                    <label className='ing-label' for='ingredient'>Ingredient Name: </label>
+                    <input onChange={handleIngChange} className='ingredient' type='text' id='ingredient'></input>
+                    <br />
+                    <label className='weight-label' for='weight'>Total Weight: </label>
+                    <input onChange={handleWeightChange} className='weight' type='text' id='weight'></input>
+                    <br />
+                    <label className='cost-label' for='cost'>Total Cost: $</label>
+                    <input onChange={handleCostChange} className='cost' type='text' id='cost'></input>
+                </form>
+                <button onClick={createNewIng} className='create-new'>Add Ingredient</button>
+            </div>
+            {list === [] ? listIng() : <p>no ingredients</p>}
         </div>
     )
 }
