41eaff6dddb72e571c7056efad3e5d14aa92c1a4
Colin Ciervo <colinciervo@@gmail.com>
2023-10-16 17:10:40 -0400

    Add delete functionality to the panty

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,20 @@
-import './Card.css'
+import React from 'react';
+import './Card.css';
 
-function Card() {
-    
+function Card({ name, weight, cost, id, deleteCard }) {
+    const handleDelete = () => {
+        deleteCard(id);
+    }
+
+    return (
+        <div className='card-main' key={id}>
+            <h3 className='card-title'>{name}</h3>
+            <p className='card-weight'>Weight: {weight}#</p>
+            <p className='card-cost'>Cost: ${cost}</p>
+            <br/>
+            <button className='delete-button' onClick={handleDelete}>Delete</button>
+        </div>
+    );
 }
 
-export default Card
+export default Card;
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,5 +1,6 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import './Ingredients.css'
+import Card from '../Card/Card'
 
 function Ingredients() {
     const [name, setName] = useState('')
@@ -9,35 +10,26 @@ function Ingredients() {
 
     const handleIngChange = (event) => {
         setName(event.target.value)
-        console.log(name)
     }
 
     const handleWeightChange = (event) => {
-        let weightNumber = parseInt(event.target.value)
-        if (isNaN(weightNumber)) {
-            console.log('NaN')
-        } else {
+        let weightNumber = parseInt(event.target.value);
+        if (!isNaN(weightNumber)) {
             setWeight(weightNumber)
         }
-        console.log(weightNumber)
     }
 
     const handleCostChange = (event) => {
         let costNumber = parseInt(event.target.value)
-        if (isNaN(costNumber)) {
-            console.log('NaN')
-        } else {
+        if (!isNaN(costNumber)) {
             setCost(costNumber)
         }
-        console.log(costNumber)
     }
-    
+
     const createNewIng = (event) => {
         event.preventDefault()
         const newIng = { name: name, weight: weight, cost: cost }
-        console.log(newIng)
         setList(list => [...list, newIng])
-        console.log(list)
         clearInputs()
     }
 
@@ -49,47 +41,72 @@ function Ingredients() {
 
     const listIng = () => {
         return list.map((ing, index) => (
-            <div className='list' key={index}>
-                <h3 className='list-title'>{ing.name}</h3>
-                <p>Total Weight: {ing.weight}#</p>
-                <p>Total Cost: ${ing.cost}</p>
-                <button className='list-button'>Add To current recipe</button>
-                <br/>
-                <button className='list-button'>Delete Ingredient</button>
-            </div>
+            <Card
+                key={index}
+                name={ing.name}
+                weight={ing.weight}
+                cost={ing.cost}
+                deleteCard={() => handleDeleteCard(index)}
+            />
         ))
     }
+
+    const handleDeleteCard = (index) => {
+        const updatedList = list.filter((ing, ingIndex) => ingIndex !== index)
+        setList(updatedList)
+    }
+
 
     useEffect(() => {
         const savedList = JSON.parse(localStorage.getItem('ingredientList'));
         if (savedList) {
-            setList(savedList)
+            setList(savedList);
         }
-    }, [])
+    }, []);
 
     useEffect(() => {
-        localStorage.setItem('ingredientList', JSON.stringify(list))
-    }, [list])
+        localStorage.setItem('ingredientList', JSON.stringify(list));
+    }, [list]);
 
     return (
         <div>
             <div>
                 <h3 className='ing-title'>Create New Ingredient</h3>
                 <form>
-                    <label className='ing-label' for='ingredient'>Ingredient Name: </label>
-                    <input onChange={handleIngChange} className='ingredient' type='text' id='ingredient'></input>
+                    <label className='ing-label' htmlFor='ingredient'>Ingredient Name: </label>
+                    <input
+                        onChange={handleIngChange}
+                        className='ingredient'
+                        type='text'
+                        id='ingredient'
+                        value={name}
+                    ></input>
                     <br />
-                    <label className='weight-label' for='weight'>Total Weight: </label>
-                    <input onChange={handleWeightChange} className='weight' type='text' id='weight'></input>
+                    <label className='weight-label' htmlFor='weight'>Total Weight: </label>
+                    <input
+                        onChange={handleWeightChange}
+                        className='weight'
+                        type='text'
+                        id='weight'
+                        value={weight} 
+                    ></input>
                     <br />
-                    <label className='cost-label' for='cost'>Total Cost: $</label>
-                    <input onChange={handleCostChange} className='cost' type='text' id='cost'></input>
+                    <label className='cost-label' htmlFor='cost'>Total Cost: $</label>
+                    <input
+                        onChange={handleCostChange}
+                        className='cost'
+                        type='text'
+                        id='cost'
+                        value={cost} 
+                    ></input>
                 </form>
-                <button onClick={createNewIng} className='create-new'>Add Ingredient</button>
+                <button onClick={createNewIng} className='create-new'>
+                    Add Ingredient
+                </button>
             </div>
             {list.length === 0 ? <p className='no-ing'>No Ingredients Yet!</p> : listIng()}
         </div>
-    )
+    );
 }
 
-export default Ingredients
+export default Ingredients;
